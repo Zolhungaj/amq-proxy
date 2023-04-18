f6c4a1a58465b2bbc9408d38732311e1cb58abbc
Zolhungaj <zolhungaj@gmail.com>
2023-04-18 22:07:29 +0200

    fix validation

--- src/main/java/tech/zolhungaj/amqproxy/FfmpegRequest.java
+++ src/main/java/tech/zolhungaj/amqproxy/FfmpegRequest.java
@@ -2,18 +2,19 @@ package tech.zolhungaj.amqproxy;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.URL;
-import org.springframework.validation.annotation.Validated;
 
+import java.math.BigDecimal;
 
-@Validated
 public record FfmpegRequest(
         @URL(host = "files.catbox.moe", protocol = "https")
+        @Pattern(regexp = "^[^\"]+\\.(webm|mp3)$")
         String url,
         @Min(0)
-        double start,
+        BigDecimal start,
         @Min(1)
         @Max(90)
-        double length
+        BigDecimal length
 ) {
 }
