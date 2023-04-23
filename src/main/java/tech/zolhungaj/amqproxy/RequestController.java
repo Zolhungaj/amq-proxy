8533d3336d2ac3ed58204500b23d8462543f123b
Zolhungaj <zolhungaj@gmail.com>
2023-04-23 23:18:14 +0200

    add security configuration and external path in returned link

--- src/main/java/tech/zolhungaj/amqproxy/RequestController.java
+++ src/main/java/tech/zolhungaj/amqproxy/RequestController.java
@@ -2,6 +2,7 @@ package tech.zolhungaj.amqproxy;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class RequestController {
     private final FfmpegRunner ffmpegRunner;
 
+    @Value("${amq-proxy.external-path}")
+    private String externalPath;
+
     @PostMapping("/proxy-video")
     public String proxyVideo(@Valid @RequestBody FfmpegRequest request){
-        return ffmpegRunner.copyVideo(request);
+        return externalPath + ffmpegRunner.copyVideo(request);
     }
 
     @PostMapping("/proxy-audio")
     public String proxyAudio(@Valid @RequestBody FfmpegRequest request){
-        return ffmpegRunner.extractAudioAndEncodeToMp3(request);
+        return externalPath + ffmpegRunner.extractAudioAndEncodeToMp3(request);
     }
 }
